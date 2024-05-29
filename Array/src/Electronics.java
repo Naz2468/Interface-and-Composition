11c13646f2117539e2402bfd115efd0caed35162
Naz2468 <133638898+Naz2468@users.noreply.github.com>
2024-05-29 12:21:31 +0100

    Add files via upload

---
+++ Array/src/Electronics.java
@@ -0,0 +1,16 @@
+public abstract class Electronics {
+    boolean onOrOff = false;
+
+    boolean on();
+
+    boolean off() {
+        return false;
+    }
+
+    default void electronic() {
+        if (onOrOff) {
+            System.out.println("Default");
+
+        }
+    }
+}
---
+++ Array/src/arrays.java
@@ -0,0 +1,20 @@
+public class arrays
+String word = "Waves";
+char[] array = word.toCharArray();
+int j;
+for (j = array.length - 1; j >= 0; j--) {
+        System.out.println("array"[j]);
+
+}
+        if (array[j] == word.toCharArray());{
+        System.out.println("the word is a palindrone");
+} else {
+        System.out.println("Invalid");
+        }{
+}
+
+public void main() {
+}
+
+public void main() {
+}
